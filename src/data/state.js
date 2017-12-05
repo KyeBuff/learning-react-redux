bd3cb3934d2008306cec2f283e59b01f24171f63
KyeBuff <kye.buffery@btinternet.com>
2017-12-05 14:30:47 +0000

    Set up: store, reducer; installed react-redux

---
+++ src/data/state.js
@@ -0,0 +1,40 @@
+
+import { Map, List } from "immutable";
+
+import { createStore } from "redux";
+
+import reducer from "./reducer";
+
+
+export const initial = Map({
+
+    articles: List([
+
+        Map({
+            id: 1,
+            title: "Post #1",
+            article: "<p>Blah blah blah</p>",
+            comments: List([
+                Map({ email: "bob@bob.com", comment: "Great blog post" }),
+            ]),
+            tags: List(["blah", "monkeys"]),
+        }),
+
+        Map({
+            id: 2,
+            title: "Post #2",
+            article: "<p>Blah blah blah</p>",
+            comments: List([
+                Map({ email: "sandi@sandi.com", comment: "I disagree, but not in an agressive and unpleasant manner" }),
+            ]),
+            tags: List(["blah", "fish-fingers"]),
+        })
+    ])
+});
+
+export const store = createStore(
+    reducer,
+    initial,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
--- src/index.js
+++ src/index.js
@@ -7,30 +7,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 // import the App component
 import App from "./App";
 
-// import in Immutable Map and List for our dummy data
-import { Map, List } from "immutable";
 
-// create some dummy articles
-const articles = List([
-    Map({
-        id: 1,
-        title: "Post #1",
-        article: "<p>Blah blah blah</p>",
-        comments: List([
-            Map({ email: "bob@bob.com", comment: "Great blog post" }),
-        ]),
-        tags: List(["blah", "monkeys"]),
-    }),
-    Map({
-        id: 2,
-        title: "Post #2",
-        article: "<p>Blah blah blah</p>",
-        comments: List([
-            Map({ email: "sandi@sandi.com", comment: "I disagree, but not in an agressive and unpleasant manner" }),
-        ]),
-        tags: List(["blah", "fish-fingers"]),
-    }),
-]);
+
+import { initial, store } from "./data/state";
+
+let articles = store.getState().get("articles");
 
 // wrap the app in Router
 // pass our articles through to App using props
